1d95825205d3f5a32cb205fde90faf421d9ef405
Jack Ritchie <jackritchie@Jacks-MacBook-Pro.local>
2023-03-31 15:08:39 +1100

    finsihed, now watching walkthrough to see where I could improve

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 const spacingAdj = document.querySelector('#spacing');
 const blurAdj = document.querySelector('#blur');
 const colorAdj = document.querySelector('#base');
+const img = document.querySelector('img');
+const h1 = document.querySelector('.h1');
 
-spacingAdj.addEventListener('input', ()=>{
+spacingAdj.addEventListener('input', () => {
+    let desiredSpacing = spacingAdj.value;
+    img.style.setProperty('--spacing', `${desiredSpacing}px`)
+
     
 })
 
 blurAdj.addEventListener('input', ()=>{
-    
+    let desiredBlur = blurAdj.value;
+    img.style.setProperty('--blur', `${desiredBlur}px`)
+
 })
 
 colorAdj.addEventListener('input', ()=>{
-    
+    let desiredColor = colorAdj.value;
+    console.log(desiredColor)
+    img.style.setProperty('--base', `${desiredColor}`)
+    h1.style.setProperty('--base', `${desiredColor}`)
 })
