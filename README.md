16b9bbd7d1d1792242ad3ce830463a6a24e8d7e6
Jack Ritchie <jackritchie@Jacks-MacBook-Pro.local>
2023-03-31 15:30:17 +1100

    refactored code

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # CSS-variables-with-JS
+
 Using CSS variables with JS
+
+# Outcome
+
+My code was too dense after watching the walk through it was handy to note the utility in structuring html elements with attributes such as name and dataset infomation to make scripts a lot more condense!
--- script.js
+++ script.js
@@ -1,25 +1,45 @@
-const spacingAdj = document.querySelector('#spacing');
-const blurAdj = document.querySelector('#blur');
-const colorAdj = document.querySelector('#base');
-const img = document.querySelector('img');
-const h1 = document.querySelector('.h1');
+// const spacingAdj = document.querySelector('#spacing');
+// const blurAdj = document.querySelector('#blur');
+// const colorAdj = document.querySelector('#base');
+// const img = document.querySelector('img');
+// const h1 = document.querySelector('.h1');
 
-spacingAdj.addEventListener('input', () => {
-    let desiredSpacing = spacingAdj.value;
-    img.style.setProperty('--spacing', `${desiredSpacing}px`)
+// spacingAdj.addEventListener('input', () => {
+//     let desiredSpacing = spacingAdj.value;
+//     img.style.setProperty('--spacing', `${desiredSpacing}px`)
 
     
+// })
+
+// blurAdj.addEventListener('input', ()=>{
+//     let desiredBlur = blurAdj.value;
+//     img.style.setProperty('--blur', `${desiredBlur}px`)
+
+// })
+
+// colorAdj.addEventListener('input', ()=>{
+//     let desiredColor = colorAdj.value;
+//     console.log(desiredColor)
+//     img.style.setProperty('--base', `${desiredColor}`)
+//     h1.style.setProperty('--base', `${desiredColor}`)
+// })
+
+// refactored
+
+//select all inputs
+
+const inputs = document.querySelectorAll('input');
+
+// add event listener to all input to call back handle change
+inputs.forEach(input =>{
+    input.addEventListener('input', handleChange)
 })
 
-blurAdj.addEventListener('input', ()=>{
-    let desiredBlur = blurAdj.value;
-    img.style.setProperty('--blur', `${desiredBlur}px`)
+function handleChange(){
+    //use element data attributes to get suffix if there is one...
+    const suffix = this.dataset.sizing || '';
+    //gets the :root element and changes the style using the name of element called, value and suffix if available 
+   document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)
+}
 
-})
 
-colorAdj.addEventListener('input', ()=>{
-    let desiredColor = colorAdj.value;
-    console.log(desiredColor)
-    img.style.setProperty('--base', `${desiredColor}`)
-    h1.style.setProperty('--base', `${desiredColor}`)
-})
